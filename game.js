37daf7b70e6254f104ce1e1855c068eeb17ec7e5
Nicolás Mandrile <mandrilenico@gmail.com>
2020-11-06 16:40:23 -0300

    1.9 RequestAnimationFrame: regulating the time between devices

--- game.js
+++ game.js
@@ -311,7 +311,7 @@
 //     window.addEventListener('load', init, false);
 // }(window));
 
-/* RequestAnimationFrame */
+/* RequestAnimationFrame (deltaTime) */
 'use strict';
 var canvas = null,
     ctx = null,
@@ -321,36 +321,44 @@ var canvas = null,
     acumDelta = 0,
     x = 50,
     y = 50;
+
 window.requestAnimationFrame = (function () {
     return window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame ||
         function (callback) {
             window.setTimeout(callback, 17);
-        };  
+        };
 }());
+
 function paint(ctx) {
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     ctx.fillStyle = '#0f0';
     ctx.fillRect(x, y, 10, 10);
+
     ctx.fillStyle = '#fff';
     ctx.fillText('FPS: ' + FPS, 10, 10);
 }
-function act() {
-    x += 2;
+
+function act(deltaTime) {
+    x += 120 * deltaTime;
     if (x > canvas.width) {
         x = 0;
     }
 }
+
 function run() {
     window.requestAnimationFrame(run);
+
     var now = Date.now(),
-    deltaTime = (now - lastUpdate) / 1000;
+        deltaTime = (now - lastUpdate) / 1000;
     if (deltaTime > 1) {
         deltaTime = 0;
     }
     lastUpdate = now;
+
     frames += 1;
     acumDelta += deltaTime;
     if (acumDelta > 1) {
@@ -358,12 +366,15 @@ function run() {
         frames = 0;
         acumDelta -= 1;
     }
-    act();
+
+    act(deltaTime);
     paint(ctx);
 }
+
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     run();
 }
+
 window.addEventListener('load', init, false);
